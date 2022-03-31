01d8d8efa728c9dae41a2e25c82d2a4c5e419600
Anamika_S <anamika.sawhney22@gmail.com>
2022-03-31 07:28:27 +0000

    Add project files.

---
+++ StudentDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Student.batch = "DotNet";
+            Console.WriteLine("Institue Name is " + Student.Institue);
+            //Student.Institue = "aaaaa";
+            Student.Batch();
+            // Will call Def Cons
+            Student student1 = new Student();
+            student1.GetDetails();
+            student1.DisplayDetails();
+
+
+            // Will call Para Cons
+            Student student2 = new Student(10);
+            student2.DisplayDetails();
+
+
+            // Will call Para Cons
+            Student student3 = new Student(11, "Mahesh");
+            student2.DisplayDetails();
+
+            // Will call Para Cons
+            Student student4 = new Student(12, "Deepak", 90);
+            student4.DisplayDetails();
+            
+            // Will call Copy Cons
+            Student student5 = new Student(student4);
+            student5.DisplayDetails();
+
+        }
+    }
+}
---
+++ StudentDemo/Student.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDemo
+{
+    class Student
+    {
+        readonly int id;
+        string name;
+        static string batch;
+        int score;
+        public const string Institue="ABC Institue";
+
+
+        public static void Batch()
+        {
+           
+            
+            Console.WriteLine("Batch Code is " +batch);
+        }
+
+        // Static Con > Can be only 1 within a class
+        // It shoud be parameterless , cannot have any access specefier
+        static Student()
+        {
+            batch = "B001";
+        }
+        // Def Cons  > COULD BE ONLY ONE
+        public Student() { }
+
+        //Para Cons
+        public Student(int id)
+        {
+            this.id = id;
+            Console.WriteLine("ENter Name");
+            name = Console.ReadLine();
+            //Console.WriteLine("Enter Batch");
+            //batch = Console.ReadLine();
+            Console.WriteLine("ENter Score");
+            score = byte.Parse(Console.ReadLine());
+
+        }
+        //Para Cons
+
+        public Student(int id, string name)
+        {
+            this.id = id;
+             
+            this.name = name;
+            //Console.WriteLine("Enter Batch");
+            //batch = Console.ReadLine();
+            Console.WriteLine("ENter Score");
+            score = byte.Parse(Console.ReadLine());
+
+        }
+
+        //Fully Para Cons
+
+        public Student(int id, string name, int score)
+        {
+            this.id = id;
+
+            this.name = name;
+           
+            this.score = score;
+
+        }
+
+        // Copy Cons
+        public Student(Student student)
+        {
+            Console.WriteLine("Enter ID");
+            id = Byte.Parse(Console.ReadLine());
+
+            this.name = student.name;
+            //this.batch = student.batch;
+            this.score = student.score;
+        }
+
+        public void GetDetails()
+        {
+            //Console.WriteLine("Enter ID");
+            //id = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("ENter Name");
+            name = Console.ReadLine();
+            //Console.WriteLine("Enter Batch");
+            //batch = Console.ReadLine();
+            Console.WriteLine("ENter Score");
+            score = byte.Parse(Console.ReadLine());
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Rollo is {id}");
+            Console.WriteLine($"Name is {name}");
+            //Console.WriteLine($"Batch is {batch}");
+            Console.WriteLine($"Score is {score}");
+
+        }
+    }
+}
